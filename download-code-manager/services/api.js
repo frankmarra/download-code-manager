c5e05bb4c1b43b152a8250f2b7eef8976d8a5082
Frank Marra <frankrmarra@gmail.com>
2022-07-26 18:00:29 -0400

    fixed cookie

--- download-code-manager/services/api.js
+++ download-code-manager/services/api.js
@@ -8,27 +8,29 @@ const Client = axios.create({ baseURL: BASE_URL })
 Client.interceptors.request.use(
   (config) => {
     let cookie = getCookie('user')
-    let cookieToken = JSON.parse(cookie)
-    let token = cookieToken
-    if (token) {
-      config.headers['authorization'] = `Bearer ${token.token}`
+    if (cookie) {
+      let cookieToken = JSON.parse(cookie)
+      let token = cookieToken
+      if (token) {
+        config.headers['authorization'] = `Bearer ${token.token}`
+      }
     }
     return config
   },
   (error) => Promise.reject(error)
 )
 
-Client.interceptors.response.use(
-  (config) => {
-    let cookie = getCookie('user')
-    let cookieToken = JSON.parse(cookie)
-    let token = cookieToken
-    if (token) {
-      config.headers['authorization'] = `Bearer ${token.token}`
-    }
-    return config
-  },
-  (error) => Promise.reject(error)
-)
+// Client.interceptors.response.use(
+//   (config) => {
+//     let cookie = getCookie('user')
+//     let cookieToken = JSON.parse(cookie)
+//     let token = cookieToken
+//     if (token) {
+//       config.headers['authorization'] = `Bearer ${token.token}`
+//     }
+//     return config
+//   },
+//   (error) => Promise.reject(error)
+//)
 
 export default Client

493d355e1eff15489f4a1dde19dc7cb9bd3aa75e
Frank Marra <frankrmarra@gmail.com>
2022-09-09 10:20:13 -0400

    made footer smaller.  added animated links to footer

--- download-code-manager/components/footer.js
+++ download-code-manager/components/footer.js
@@ -4,26 +4,30 @@ const year = today.getFullYear()
 const Footer = () => {
   return (
     <footer>
-      <div>
-        <p>© {year} downloadcodemanager.com</p>
+      <div style={{ textAlign: 'center' }}>
+        <p>
+          <a
+            href="https://mysterycircles.com/donate-to-mc"
+            target="_blank"
+            rel="noopener"
+            className="footer-link"
+          >
+            Buy us a coffee?{' '}
+          </a>
+        </p>
       </div>
       <div style={{ textAlign: 'center' }}>
         <a
           href="mailto:mysterycircles@gmail.com?subject=I%20am%20interested%20in%20joining"
           target="_blank"
           rel="noopener"
+          className="footer-link"
         >
           Interested in joining?{' '}
         </a>
       </div>
-      <div style={{ textAlign: 'center' }}>
-        <a
-          href="https://mysterycircles.com/donate-to-mc"
-          target="_blank"
-          rel="noopener"
-        >
-          Buy us a coffee?{' '}
-        </a>
+      <div>
+        <p>© {year} downloadcodemanager.com</p>
       </div>
     </footer>
   )
